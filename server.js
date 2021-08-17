e3b5ef93eddb68b97ba1db5bdab47e7ce93cd18a
Segrey Seleznev <serega210699@mail.ru>
2021-08-17 15:43:41 +0500

    make it better

--- server.js
+++ server.js
@@ -55,5 +55,5 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 http.listen(port, () => {
-  console.log("Server is running on port", port);
+  console.log(`Open in your browser http://localhost:${port}/`);
 });
